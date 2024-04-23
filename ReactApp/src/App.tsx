88e16912128d3c0df4d394d9cacdc4b4250e525a
yashr775 <rajy29629@gmail.com>
2024-04-23 16:06:52 +0530

    Basic completed

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,9 +1,39 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*_-+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-black w-screen h-screen p-5">
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 p-5">
+          <div className="flex shadow rounded-lg overflow-hidden mb-4">
+            <input
+              type="text"
+              value={password}
+              className="outline-none  w-full px-3 py-1 "
+              placeholder="password"
+            ></input>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
